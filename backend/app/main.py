e764190eb0e10edd0134a9407e69b53c98227260
Abhay Shinde <abhayshinde16325@gmail.com>
2026-06-02 10:04:36 +0530

    setup mongodb atlas connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from app.database.connection import client
+
 
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_db_check():
+    try:
+        client.admin.command('ping')
+        print("Connected to MongoDB successfully!")
+    except Exception as e:
+        print(f"Error connecting to MongoDB: {e}")
+
 
 @app.get("/")
 def home():
